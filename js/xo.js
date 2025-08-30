5bcd50bfa9828c89ed882d817e0e47054d84e22c
Danil <dvbagin@gmail.com>
2025-08-30 14:13:30 +0700

    Добавлены обработчики кнопок ИГРАТЬ и НАСТРОЙКИ

--- js/xo.js
+++ js/xo.js
@@ -29,5 +29,25 @@ const xo = {
             document.getElementById('player1').value = '';
         }
         
+    },
+    showSettings(){
+        document.getElementById("settings").style.display = "inline-block";
+        document.getElementById("game").style.display = "none";
+        document.getElementById("info").style.display = "none";
+    },
+    setName(){
+        for(let i = 0; i < 2; i++){
+            let name = document.getElementById("player" + i).value;
+            if(name === ''){
+                this.players[i].name = "Игрок "+(i + 1);
+                document.getElementById("player" + i).value = "Игрок "+(i + 1);
+            }
+            else{ 
+                this.players[i].name = name;
+            }
+        }
+        document.getElementById("settings").style.display = "none";
+        document.getElementById("game").style.display = "inline-block";
+        document.getElementById("info").style.display = "inline-block";
     }
 } 
